5ec011f4fe00c082ca8d8f5af09a98bbfcdf40b6
Lawrence Yu <lawy888@gmail.com>
2019-10-14 08:31:35 -0700

    add blockmap implementation

---
+++ blockmap.py
@@ -0,0 +1,114 @@
+class Blockmap():
+    """ class used to keep track of which blocks in the file have been downloaded, which are currently pending download
+        by which connections, and which blocks are available.
+        
+        The blockmap is always read from disk and never kept in memory
+    """
+    DOWNLOADED = '*'
+    AVAILABLE = '.'
+    PENDING = '0123456789ABCDEF'
+    
+    def __init__(self, remote_path, local_path, file_size_func):
+        """ initialize the blockmap
+        
+            Check if a local blockmap exists in the local_path location, if it does not exist, try to contact the FTP
+            server to get the filesize and then create a new blockmap
+        
+            Args:
+                remote_path - path on ftp server of file to download
+                local_path - local path on disk where downloaded file will be saved
+                file_size_func - a function that can be called to get the file size of the file on the FTP server, the
+                                 prototype for this function is file_size_func(remote_path) returns an int
+        """     
+        self._blocksize = 1024 * 1024
+        self._remote_path = remote_path
+        self._local_path = local_path
+
+        # generate the blockmap_path
+        if os.path.isdir(local_path):
+            raise Exception('Error! local path "%s" is a directory, must be a file' % local_path)        
+        self._blockmap_path = self._local_path + '.blockmap'
+
+        # create a new blockmap if it does not exist saved to disk
+        if not os.path.exists(self._blockmap_path):
+            # create a new blockmap and save it
+            filesize = file_size_func(self._remote_path)
+            blockmap = self.AVAILABLE * int(math.ceil(filesize / float(self._blocksize)))
+            self._persist_blockmap(blockmap)
+
+    def _read_blockmap(self):
+        """ load the blockmap from the local copy """
+        with open(self._blockmap_path, 'r') as f:
+            return f.read()
+            
+    def _persist_blockmap(self, blockmap):
+        """ save the blockmap to disk """
+        with open(self._blockmap_path, 'w') as f:
+            f.write(blockmap)
+
+    def allocate_segment(self, worker_id):
+        """ allocate an available segment in the blockmap to the specified worker_id
+        
+            Args:
+                worker_id - a worker id to allocate the segment to
+            
+            Returns:
+                (starting_block, blocks)
+        """
+        # clean the blockmap of any pending blocks with this worker_id
+        self.clean_blockmap(worker_id)
+        
+        # read the blockmap and look for the longest contiguous chain of available blocks
+        blockmap = self.read_blockmap()
+        for i in range(self.max_segment_size, 0, -1):
+            blocks = i
+            start_block = blockmap.find('.' * i)
+
+        # try to allocate the segment
+        byte_offset = start_block * self._blocksize
+        self.change_block_range_status(byte_offset, blocks, worker_id)
+        
+        # return the allocated segment
+        return byte_offset, blocks
+
+    def change_block_range_status(self, byte_offset, blocks, status):
+        """ change the status of a block range
+        
+            Args:
+                byte_offset - starting byte offset of the block.  NOTE: This is not a block number but a byte offset!
+                blocks - number of blocks to change status of.  NOTE: This is number of blocks, not bytes!
+                status - new status
+        """
+        # make sure byte_offset is a multiple of block size
+        if byte_offset % self._blocksize != 0:
+            raise Exception('byte_offset %d is not a multiple of block size %d' % (byte_offset, self._blocksize))
+
+        # make sure status is valid
+        if status not in (self.AVAILABLE, self.DOWNLOADED):
+            raise Exception('status of "%s" is not a valid status' % status) 
+            
+        # set the status of the block
+        blockmap = self._load_blockmap()
+        starting_block = byte_offset / self._blocksize
+        for i in range(0, blocks):
+            blockmap[starting_block + i] = status
+        self._persist_blockmap(blockmap)
+
+    def clean_blockmap(self, worker_id):
+        """ clean any pending blocks from the blockmap """
+        # check that worker id is a 
+        if worker_id not in self.PENDING:
+            raise Exception('"%s" is not a valid worker_id' % worker_id)
+ 
+        blockmap = self._read_blockmap()
+        blockmap = blockmap.replace(worker_id, self.AVAILABLE)
+        self._persist_blockmap()
+
+    def delete_blockmap(self):
+        """ delete the blockmap """
+        os.remove(self._blockmap_path)
+
+    def is_blockmap_complete(self):
+        """ returns true if the blockmap shows that the entire file has been downloaded """
+        blockmap = self._read_blockmap()
+        return set(blockmap) == set('*')
--- ftp_file_download_manager.py
+++ ftp_file_download_manager.py
@@ -17,71 +17,58 @@ class FtpFileDownloader:
         self._password = password
         self._blocksize = 1024 * 1024
 
-    def _download_segment(self):
-        pass
- 
-    def _allocate_segment(self, block_map):
-        pass
- 
-    def _activate_download_thread(self, thread_id, segment):
-        pass
-
-    def _get_blockmap_for_file(self, remote_path, local_path):
-        """ Retrieves the blockmap for a file.  The local_path is first checked for an existing blockmap.
-            If there is an existing blockmap, it is assumed that this is a continuation of a previously aborted
-            download.  If no blockmap exists at the local_path, a new blockmap is created and written to the 
-            local_path location.
-            
-            The remote server must be contacted to retrieve the size of the remote file in
-            order to create a new blockmap.
-            
-            Args:
-                remote_path - path to the remote file
-                local_path - file location to save the remote file to
-            
-            Returns:
-                blockmap as a string
-        """
-        # calculate the blockmap location based on the local_path
-        blockmap_path = local_path + '.blockmap'
-        
-        # read the existing blockmap if it exists already
-        if os.path.exists(blockmap_path):
-            with open(blockmap_path, 'r') as f: 
-                return f.read()
-        
-        # calculate a new blockmap, query the filesize from the remote ftp server
-        ftp = FTP(self._server_url)
-        ftp.login(self._username, self._password)
-#        ftp.prot_p()
-        print ftp.retrlines('LIST')
-        ftp.sendcmd("TYPE i")    # Switch to Binary mode
-        size = ftp.size(remote_path)   # Get size of file
-        print size        
-        ftp.sendcmd("TYPE A")    # Switch to Binary mode
-        blockmap = '.' * int(math.ceil(size / float(self._blocksize)))
-        
-        # write the new blocmap to disk
-        with open(blockmap_path, 'w') as f:
-            f.write(blockmap)
-        
-        # return the blockmap
-        return blockmap
-    
     def _get_download_thread_state(self, thread_id):
         pass
 
-    def _is_download_finished(self, blockmap):
-        """ return True if the blockmap shows that all blocks have been downloaded
-        
-            Args:
-                blockmap - blockmap string
-            
-            Returns:
-                True if the blockmap shows all blocks have been downloaded
-        """
-        return set(blockmap) == set('*')
+    def tw_ftp_download_segment(self, com_queue_in, com_queue_out, remote_path, byte_offset, blocks, blocksize,
+                                worker_id):
+        """ ??? """
+        # open an ftp connection to the server
+        with closing(ftplib.FTP(self._server_url)) as ftp:
+            # connect and login
+            ftp.connect(self._port)
+            ftp.login(self._username, self._password)
+
+            # switch to FTP binary mode, then initiate a transfer starting at the byte_offset
+            ftp.voidcmd('TYPE I')
+            conn = ftp.transfercmd('retr %s' % remote_path, byte_offset)
+            conn.settimeout(30)            
     
+            # loop until the correct amount of data has been transferred
+            bytes_received = 0
+            data = ''
+            while bytes_received < (blocks * blocksize):
+                # check for a message on in the incoming communication queue
+                while not com_queue_in.empty():
+                    msg = com_queue_in.get_nowait()
+                    if msg['type'] == 'kill':
+                        return
+                    else:
+                        raise Exception('Unhandled incoming message type of "%s"' % msg['type'])
+                
+                # receive the data
+                chunk = conn.recv(blocksize)
+                
+                # EOF if chunk is empty
+                if not chunk:
+                    # EOF, so break out of the loop and notify about the final partial block of data
+                    break
+            
+                # save the data
+                data = data + chunk
+                
+                # send data if greater than blocksize
+                if len(data) > blocksize:
+                    block = data[:blocksize]
+                    data = data[blocksize:]
+                    com_queue_out.put({'type': 'data_received', 'worker_id': worker_id,
+                                       'byte_offset': byte_offset, 'data': block})
+                    byte_offset = bytes_offset + blocksize        
+            
+            # send the final data
+            if data:
+                com_queue_out.put({'type': 'data_received', 'byte_offset': byte_offset, 'data': data})
+                    
     def download_file(self, remote_path, local_path):
         """ downloads a file from a remote ftp server
         
@@ -100,18 +87,89 @@ class FtpFileDownloader:
             local_path = os.path.join(local_path, os.path.basename(remote_path))
 
         # load an existing block map or generate a block map for the file
-        block_map = self._get_blockmap_for_file(remote_path, local_path)
-
+        blockmap = BlockMap(remote_path, local_path, ftp_get_filesize)
+        
         # loop until file is downloaded
-        while not self._finished(block_map):
+        while not blockmap.is_blockmap_complete():
             # look for an idle download thread
             for i in range(0, self._max_download_threads):
                 if self._get_download_thread_state(i) == 'IDLE':
-                    segment = self._allocate_segment(block_map)
-#                     self._activate_download_thread(i, segment)
-#             
+                    worker_id = str(i)
+                    byte_offset, blocks = blockmap.allocate_segment(worker_id)
+
+            # check the incoming com queue for messages from the download threads
+            
+
             # sleep
             time.sleep(1)
         
         # clean up the block map
-#        self._delete_block_map_for_file()
+        blockmap.delete_blockmap()
+
+
+
+
+
+#     def _download_segment(self):
+#         pass
+#  
+#     def _allocate_segment(self, block_map):
+#         pass
+#  
+#     def _activate_download_thread(self, thread_id, segment):
+#         pass
+# 
+# #     def _get_blockmap_for_file(self, remote_path, local_path):
+# #         """ Retrieves the blockmap for a file.  The local_path is first checked for an existing blockmap.
+# #             If there is an existing blockmap, it is assumed that this is a continuation of a previously aborted
+# #             download.  If no blockmap exists at the local_path, a new blockmap is created and written to the 
+# #             local_path location.
+# #             
+# #             The remote server must be contacted to retrieve the size of the remote file in
+# #             order to create a new blockmap.
+# #             
+# #             Args:
+# #                 remote_path - path to the remote file
+# #                 local_path - file location to save the remote file to
+# #             
+# #             Returns:
+# #                 blockmap as a string
+# #         """
+# #         # calculate the blockmap location based on the local_path
+# #         blockmap_path = local_path + '.blockmap'
+# #         
+# #         # read the existing blockmap if it exists already
+# #         if os.path.exists(blockmap_path):
+# #             with open(blockmap_path, 'r') as f: 
+# #                 return f.read()
+# #         
+# #         # calculate a new blockmap, query the filesize from the remote ftp server
+# #         ftp = FTP(self._server_url)
+# #         ftp.login(self._username, self._password)
+# # #        ftp.prot_p()
+# #         print ftp.retrlines('LIST')
+# #         ftp.sendcmd("TYPE i")    # Switch to Binary mode
+# #         size = ftp.size(remote_path)   # Get size of file
+# #         print size        
+# #         ftp.sendcmd("TYPE A")    # Switch to Binary mode
+# #         blockmap = '.' * int(math.ceil(size / float(self._blocksize)))
+# #         
+# #         # write the new blocmap to disk
+# #         with open(blockmap_path, 'w') as f:
+# #             f.write(blockmap)
+# #         
+# #         # return the blockmap
+# #         return blockmap
+
+
+#     def _is_download_finished(self, blockmap):
+#         """ return True if the blockmap shows that all blocks have been downloaded
+#         
+#             Args:
+#                 blockmap - blockmap string
+#             
+#             Returns:
+#                 True if the blockmap shows all blocks have been downloaded
+#         """
+#         return set(blockmap) == set('*')
+    
